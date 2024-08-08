75ba8611bffae13a22cb3083804f8e02951bac1e
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-08 17:29:29 +0330

    feat: status component

--- src/components/city/City.jsx
+++ src/components/city/City.jsx
@@ -7,7 +7,7 @@ export default function City() {
   async function queryFn() {
     try {
       const res = await axios.get(
-        "https://api.openweathermap.org/data/2.5/weather?lat=51.5073219&lon=-0.1276474&appid=7094ba1f421eb17cf9ab1b1781e963dc"
+        `https://api.openweathermap.org/data/2.5/weather?lat=51.5073219&lon=-0.1276474&appid=${process.env.REACT_APP_API_KEY}`
       );
       return res.data;
     } catch (error) {
@@ -21,9 +21,9 @@ export default function City() {
   });
   console.log(data);
   return (
-    <div className="flex justify-center items-center gap-4 bg-[#5D69F6] min-h-screen bg-[url('/bg.svg')] bg-cover">
+    <div className="flex justify-center items-center gap-8 bg-[#5D69F6] min-h-screen bg-[url('/bg.svg')] bg-cover">
       <Detail data={data} />
-      <Status />
+      <Status status={data?.weather[0]?.main} />
     </div>
   );
 }
